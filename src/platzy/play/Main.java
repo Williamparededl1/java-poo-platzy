cfd98459f4d471417a4663f1a79638f8e2355158
Williamparededl1 <wparedesl1995@gmail.com>
2026-09-10 20:55:32 -0500

    Cambios prueba

--- src/platzy/play/Main.java
+++ src/platzy/play/Main.java
@@ -10,7 +10,7 @@ public class Main {
         System.out.println("PELICULAS !!!!! ");
 
         Pelicula pelicula = new Pelicula();
-        pelicula.titulo = "La Era del hielo";
+        pelicula.titulo = "La Era del hielo en el git";
         pelicula.anioEstreno = 2002;
         pelicula.genero = "Animacion";
         pelicula.calificar(4.5);
@@ -25,7 +25,7 @@ public class Main {
 //        Scanner scanner  = new Scanner(System.in);
 //
 //        System.out.println("Cual es tu nombre?");
-//
+//`
 //        String nombre = scanner.nextLine();
 //
 //        System.out.println("Ya era hora de aprender java "+ nombre +" buen viaje");
