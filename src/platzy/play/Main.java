e6cfb8c750b86d5e6ae2bd8c875bbb7221e383e1
Williamparededl1 <wparedesl1995@gmail.com>
2026-09-10 21:16:34 -0500

    preeuba master rama 2

--- src/platzy/play/Main.java
+++ src/platzy/play/Main.java
@@ -10,7 +10,7 @@ public class Main {
         System.out.println("PELICULAS !!!!! ");
 
         Pelicula pelicula = new Pelicula();
-        pelicula.titulo = "La Era del hielo rama 2";
+        pelicula.titulo = "La Era del hielo rama 2 para la prueebaa";
         pelicula.anioEstreno = 2002;
         pelicula.genero = "Animacion";
         pelicula.calificar(4.5);
